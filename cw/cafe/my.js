3eea6aa3580b0a3a345d026f6838375140ad6384
menachemio <github@menachem.io>
2018-03-16 00:07:31 -0400

    Update my.js

--- cw/cafe/my.js
+++ cw/cafe/my.js
@@ -1,5 +1,14 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
+function showStudent() {
+    var x = document.getElementById("id_10");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showAdult() {
+    var x = document.getElementById("id_9");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
